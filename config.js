3e825c87b9b9af9afa0f0ec1a3c55e3ed18d6b92
blaze66 <dofthgumyxd@gmail.com>
2021-10-26 15:51:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 userbot = {
  owner: [
+  "6283102176419",
   "6282298069899", 
   "62822980698995",
   "6281325994066"
@@ -8,7 +9,7 @@ userbot = {
    wait: "tunggu sedang di proses...",
    error: "errorr!!"
    },
-   prefix: ["#"],
+   prefix: [","],
     setting: {
     admin: "only admin",
     group: "only group",
